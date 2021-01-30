9cd34153e50b2c304cfb8f339694daf0dfd2d270
Aviral <aviral.sit@gmail.com>
2021-01-30 21:45:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import "./App.css";
 
-const PostStore = React.createContext();
+const PostStore = createContext();
 
 const PostList = () => {
   const data = useContext(PostStore);
-  console.log(data);
-  return <div onClick={()=>data.setText('aviral')}>PostList</div>;
+  // console.log(data);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => data.setPostList(json));
+  }, []);
+  return (
+    <div>
+      <h4>PostList</h4>
+      <ul>
+        {data.postList &&
+          data.postList.map((post) => (
+            <li key={post.id} onClick={() => data.setSelectedId(post.id)}>
+              {post.title}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 const PostDetail = () => {
-  return <div>PostDetail</div>;
+  const data = useContext(PostStore);
+  useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts/" + data.selectedId)
+        .then((response) => response.json())
+        .then((json) => data.setPostDetail(json));
+  }, [data.selectedId]);
+  if (data.selectedId === null) return <div>Select a post to see detail</div>;
+  return (
+    <div>
+      <h4>PostDetail</h4>
+      <div>{data.postDetail.body}</div>
+    </div>
+  );
 };
 
 function App() {
-  const [text, setText] = useState('default');
+  const [postList, setPostList] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const [postDetail, setPostDetail] = useState(null);
   return (
     <div className="App">
-      <PostStore.Provider value={{ text, setText }}>
+      <PostStore.Provider
+        value={{
+          postList,
+          setPostList,
+          selectedId,
+          setSelectedId,
+          postDetail,
+          setPostDetail,
+        }}
+      >
         <PostList />
+        <PostDetail />
       </PostStore.Provider>
-      <PostDetail />
     </div>
   );
 }
